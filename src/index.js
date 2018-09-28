1c8ab49bfa65d8ead0495def989459dff9a5e91d
Marry705 <marry705chilo@gmail.com>
2018-09-28 15:45:53 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = function longestConsecutiveLength(array = []) {
+  let len = array.length;
+  let leng = (len === 0)?0:1;
+  let lengmax = (len === 0)?0:1;
+  array.sort((left, right) => left - right);
+    for (let i = 0; i < len-1; i++){ 
+        if (array[i+1]-array[i] === 1) {
+            leng++; 
+            lengmax = (leng>=lengmax)?leng:lengmax; 
+        }
+        else { if (array[i+1] === array[i]) lengmax = (leng>=lengmax)?leng:lengmax; 
+              else leng = 1;
+        }   
+    } 
+  return lengmax;
 }
